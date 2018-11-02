439730db1930ae5626878d281fdd8ddfd9631465
Ricardo Garcia <ricardoaugustogarcia@hotmail.com>
2018-11-02 14:20:11 -0300

    Autenticação com JWT

--- src/routes/billingCycleRoutes.js
+++ src/routes/billingCycleRoutes.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const BillingCycle = require("../controller/billingCycleController");
+const User = require("../controller/userController");
+const auth = require("../middlewares/auth");
 
 const router = express.Router();
+const authRouter = express.Router();
 
 module.exports = app => {
+  app.use("/oapi/auth", authRouter);
+
+  authRouter.post("/login", User.login);
+  authRouter.post("/signup", User.signup);
+  authRouter.post("/validateToken", User.validateToken);
+
   app.use("/api", router);
+
+  router.use(auth);
+
   BillingCycle.register(router, "/billingCycles");
 };
